761a2c00bbda9fc5c9b7d7941f9e2a47d57b1ae8
Estudiante <Estudiante@10.203.2.220>
2018-08-13 18:35:55 +0000

    Hola mundo

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Estudiante
+ *
+ * Created on 13 de agosto de 2018, 06:30 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hola mundo";
+    return 0;
+}
+
